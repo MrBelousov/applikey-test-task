828259f9ff14bf89d45e3803dbffb424e62ea017
MrBelousov <belousov.web@mail.ru>
2016-02-17 12:06:13 +0200

    Uploading user avatars without gems

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
+  before_action :set_user , only: [:show, :edit, :upload, :update, :destroy]
+
   def index
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def new
@@ -18,15 +24,37 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    upload
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def delete
   end
 
+  # File upload
+  def upload
+    uploaded_io = params[:user][:avatar]
+    File.open(Rails.root.join('public', 'uploads', 'images', uploaded_io.original_filename), 'wb') do |file|
+      file.write(uploaded_io.read)
+    end
+    @user.avatar = "/uploads/images/" + uploaded_io.original_filename
+  end
+
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
