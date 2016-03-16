56e2be5d5a5e115d7d7f4179506fef32bd9f3a34
Michail Belousov <belousov.web@mail.ru>
2016-03-16 10:47:29 +0200

    Remove deleted files from git

--- app/models/sub_comment.rb
+++
@@ -1,9 +0,0 @@
-class SubComment < ActiveRecord::Base
-  # Relationships
-  belongs_to :user
-  belongs_to :post_comment
-
-  # Validations
-  validates :comment_text, presence: true, length: { maximum: 140 }
-  validates_presence_of :user_id
-end
