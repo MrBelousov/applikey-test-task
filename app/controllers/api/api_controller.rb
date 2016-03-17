a8a1144120b170560e42e7d2565a5b1fcf2c90cb
Michail Belousov <belousov.web@mail.ru>
2016-03-17 16:30:25 +0200

    Disabled CSRF token

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,4 +1,14 @@
 class Api::APIController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :restrict_access, only: [:create, :update, :destroy]
+
+  # Disabling CSRF token for mobile applications
+  protect_from_forgery
+  skip_before_action :verify_authenticity_token, if: :json_request?
+
+  protected
+
+  def json_request?
+    request.format.json?
+  end
 end
