85a692505ba366ee6cbd8a8f3bf4eeccb14e92d7
Michail Belousov <belousov.web@mail.ru>
2016-03-28 17:58:23 +0300

    Changed schema migrations

--- db/migrate/20160318142706_add_foreign_keys_to_models.rb
+++ db/migrate/20160318142706_add_foreign_keys_to_models.rb
@@ -1,6 +1,5 @@
 class AddForeignKeysToModels < ActiveRecord::Migration
   def change
-    add_foreign_key :omniauth_fields, :users
     add_foreign_key :posts, :users
     add_foreign_key :comments, :users
     add_foreign_key :api_keys, :users
---
+++ db/schema.rb
@@ -0,0 +1,68 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160328145648) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "api_keys", force: :cascade do |t|
+    t.string   "token"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "expires_at"
+  end
+
+  add_index "api_keys", ["token"], name: "index_api_keys_on_token", using: :btree
+  add_index "api_keys", ["user_id"], name: "index_api_keys_on_user_id", using: :btree
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "text"
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.integer  "user_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  add_index "comments", ["commentable_id"], name: "index_comments_on_commentable_id", using: :btree
+  add_index "comments", ["commentable_type"], name: "index_comments_on_commentable_type", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "post_text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
+
+  create_table "users", id: :bigserial, force: :cascade do |t|
+    t.string   "name"
+    t.string   "avatar"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "remember_token"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
+
+  add_foreign_key "api_keys", "users"
+  add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users"
+end
