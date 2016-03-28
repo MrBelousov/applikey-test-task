459a19ca829b363d9b8a7dfbac3738fee1f14e6a
Michail Belousov <belousov.web@mail.ru>
2016-03-28 18:16:12 +0300

    Added production and saging rb files to unicorn

---
+++ config/unicorn/production.rb
@@ -0,0 +1,48 @@
+# Set your full path to application.
+app_path = File.expand_path('../../', File.dirname(__FILE__))
+
+# Set unicorn options
+worker_processes 5
+preload_app true
+timeout 30
+listen "127.0.0.1:5020"
+
+# Spawn unicorn master worker for user apps (group: apps)
+user 'apps', 'apps'
+
+# Fill path to your app
+working_directory app_path
+
+ENV['BUNDLE_GEMFILE'] = "#{app_path}/Gemfile"
+require 'bundler/setup'
+
+# Should be 'production' by default, otherwise use other env
+rails_env = ENV['RAILS_ENV'] || 'production'
+
+# Log everything to one file
+stderr_path "#{app_path}/log/unicorn.log"
+stdout_path "#{app_path}/log/unicorn.log"
+
+# Set master PID location
+pid "#{app_path}/tmp/pids/unicorn.pid"
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.establish_connection
+  child_pid = server.config[:pid].sub('.pid', ".#{worker.nr}.pid")
+  system("echo #{Process.pid} > #{child_pid}")
+end
